edd254219eac59ba4464106b7ba6a1257c0a8c5e
Rodrigo Oliveira <rodrigopoliveira1@gmail.com>
2020-05-28 14:18:20 -0300

    Implementacao de atualizacao de Lancamentos

--- src/main/java/com/example/algamoney/api/service/LancamentoService.java
+++ src/main/java/com/example/algamoney/api/service/LancamentoService.java
@@ -2,6 +2,7 @@ package com.example.algamoney.api.service;
 
 import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,6 +27,34 @@ public class LancamentoService {
 			throw new PessoaInexistenteOuInativaException();
 		}
 		return lancamentoRepository.save(lancamento);
+	}
+	
+	public Lancamento atualizar(Long id, Lancamento lancamento) {
+		Lancamento lancamentoSalva = buscarLancamentoExistente(id);
+		if(!lancamento.getPessoa().equals(lancamentoSalva.getPessoa())) {
+			validarPessoas(lancamento);
+		}
+		
+		BeanUtils.copyProperties(lancamento, lancamentoSalva, "id");
+		
+		return lancamentoRepository.save(lancamentoSalva);
+	}
+
+	private void validarPessoas(Lancamento lancamento) {
+		Optional<Pessoa> pessoa = null;
+		if(lancamento.getPessoa().getId() != null) {
+			pessoa = pessoaRepository.findById(lancamento.getPessoa().getId());
+		}
+		
+		if(!pessoa.isPresent() || pessoa.get().isInativo()) {
+			throw new PessoaInexistenteOuInativaException();
+		}
+		
+	}
+
+	private Lancamento buscarLancamentoExistente(Long id) {
+		Lancamento lancamentoSalvo = lancamentoRepository.findById(id).orElseThrow(() -> new IllegalArgumentException());
+		return lancamentoSalvo;
 	}
 
 }
